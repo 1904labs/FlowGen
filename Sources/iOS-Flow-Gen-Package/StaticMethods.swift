a4d7d78fe1dd644df0eb381a1b6e095d1bf05195
Connor Besancenez <cbesancenez@1904labs.com>
2020-08-05 13:18:50 -0500

    Test

---
+++ Sources/iOS-Flow-Gen-Package/StaticMethods.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Connor Besancenez on 8/5/20.
+//
+
+class StaticMethods {
+    static func printTest() {
+        print("Test")
+    }
+}
--- Sources/iOS-Flow-Gen-Package/iOS_Flow_Gen_Package.swift
+++
@@ -1,3 +0,0 @@
-struct iOS_Flow_Gen_Package {
-    var text = "Hello, World!"
-}
