68b92262a78eb435930d8cb6a8f6796c697d6252
Connor Besancenez <cbesancenez@1904labs.com>
2020-08-06 14:00:57 -0500

    Changes

--- Sources/iOS-Flow-Gen-Package/Signinable.swift
+++ Sources/iOS-Flow-Gen-Package/Signinable.swift
@@ -8,6 +8,9 @@
 import Combine
 
 public protocol Signinable: class {
+    var cancellables: Set<AnyCancellable>
+
+    // Required fields
     var username: String? { get }
     var usernamePublished: Published<String?> { get }
     var usernamePublisher: Published<String?>.Publisher { get }
@@ -15,5 +18,29 @@ public protocol Signinable: class {
     var password: String? { get }
     var passwordPublished: Published<String?> { get }
     var passwordPublisher: Published<String?>.Publisher { get }
+        
+    // Validaton publishers
+    var usernameIsValidPublisher: Published<Bool>.Publisher
+    var passwordIsValidPublisher: Published<Bool>.Publisher
+    var signInValidPublisher: Published<Bool>.Publisher
+    
+    // State
+    var signInIsActive: Bool { get }
+    var signInIsActivePublished: Published<Bool> { get }
+    var signInIsActivePublisher: Published<String?>.Publisher { get }
+    
+    var isSignedIn: Bool { get }
+    var isSignedInPublished: Published<Bool> { get }
+    var isSignedInPublisher: Published<String?>.Publisher { get }
+}
+
+extension Signinable {
+    var signInValidPublisher = {
+        Publishers.CombineLatest($usernameIsValidPublisher, passwordIsValidPublisher)
+            .map { usernameIsValid, passwordIsValid in
+                return usernameIsValid && passwordIsValid
+            }
+            .eraseToAnyPublisher()
+    }
 }
 #endif
