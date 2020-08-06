f1e796377a170a27144681b73ee9b76092e3c149
Connor Besancenez <cbesancenez@1904labs.com>
2020-08-06 16:55:23 -0500

    Executable

--- Package.swift
+++ Package.swift
@@ -8,9 +8,8 @@ let package = Package(
     platforms: [ .iOS(.v13) ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "FlowGenCore",
-            targets: ["FlowGenCore"]),
+    .library(name: "FlowGenCore", targets: ["FlowGenCore"]),
+    .executable(name: "FlowGen", targets: ["FlowGen"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
